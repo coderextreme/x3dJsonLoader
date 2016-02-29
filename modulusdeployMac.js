bd4cba7c992a0160cf0ab41beacbea648ee33d7e
John Carlson <yottzumm@gmail.com>
2016-02-29 03:13:36 -0500

    Got rid of script I just added.

--- modulusdeployMac.js
+++
@@ -1,7 +0,0 @@
-const spawn = require('child_process').spawn;
-
-const modulus = spawn('modulus', ['deploy'], { stdio: 'inherit'} );
-
-modulus.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
--- modulusdeployWin.js
+++
@@ -1,7 +0,0 @@
-const spawn = require('child_process').spawn;
-
-const modulus = spawn('modulus.cmd', ['deploy'], { stdio: 'inherit'} );
-
-modulus.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
