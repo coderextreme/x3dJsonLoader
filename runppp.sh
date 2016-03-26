cf7312baa15f7e7fb3d4ed76a7bb535c1bec3c09
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2016-03-26 05:02:16 -0400

    Updated

--- runppp.sh
+++ runppp.sh
@@ -1,25 +1,23 @@
-#!/bin/bash
-node PPP.js < public/ArchPrototype.json > public/ArchPrototypes.json 
+#!/bin/sh -x
+node PPP.js public/for.json > public/fors.json
 read me
-node PPP.js < public/ObliqueStrategies.json > public/ObliqueStrategiess.json 
+node PPP.js public/rubikOnFire.json > public/rubikOnFires.json
 read me
-node PPP.js < public/bubbles.json > public/bubbless.json 
+node PPP.js public/bubbles.json > public/bubbless.json 
 read me
-node PPP.js < public/for.json > public/fors.json
+node PPP.js public/ArchPrototype.json > public/ArchPrototypes.json 
 read me
-node PPP.js < public/rubikOnFire.json > public/rubikOnFires.json
+node PPP.js public/ObliqueStrategies.json > public/ObliqueStrategiess.json 
 read me
-node PPP.js < public/rubik.json > public/rubiks.json
+node PPP.js public/rubik.json > public/rubiks.json
 read me
-node PPP.js < public/pp3.json > public/pp3s.json
+node PPP.js public/pp3.json > public/pp3s.json
 read me
-node PPP.js < public/qq3.json > public/qq3s.json
+node PPP.js public/qq3.json > public/qq3s.json
 read me
-node PPP.js < public/cobweb.json > public/cobwebs.json 
+node PPP.js public/cobweb.json > public/cobwebs.json 
 read me
-node PPP.js < public/force.json > public/forces.json
+node PPP.js public/force.json > public/forces.json
 read me
-node PPP.js < public/NancyPrototypes.json > public/NancyPrototypess.json
-read me
-node PPP.js < public/rubikFurnace.json > public/rubikFurnaces.json
+node PPP.js public/NancyPrototypes.json > public/NancyPrototypess.json
 read me
